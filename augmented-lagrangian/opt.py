6fac15d2fd95eb89cf440dcdd19de957daf29c20
Ryan Cotterell <ryan.cotterell@gmail.com>
2017-05-02 04:40:32 -0400

    fixed mus

--- augmented-lagrangian/opt.py
+++ augmented-lagrangian/opt.py
@@ -5,9 +5,9 @@ from numpy import zeros, ones, zeros_like, ones_like, maximum
 def func(X, W, theta, H, L, Mu_W,  Mu_theta, Mu_H):
     estim = W.dot(np.diag(theta)).dot(H)
     return ((X - estim)**2).sum() + L * np.abs(theta).sum() \
-        + ((Mu_W/2.0) * np.maximum(W, 0)**2).sum() \
-        + ((Mu_H/2.0) * np.maximum(H, 0)**2).sum() \
-        + ((Mu_theta/2.0) * np.maximum(theta, 0)**2).sum() 
+        + 1.0/2.0 * (np.maximum(W + Mu_W, 0)**2).sum() \
+        + 1.0/2.0 * (np.maximum(H + Mu_H, 0)**2).sum() \
+        + 1.0/2.0 * (np.maximum(theta + Mu_theta, 0)**2).sum() 
 
 
 def fd_W(X, W, theta, H, L, Mu_W,  Mu_theta, Mu_H, eps=1.0):
@@ -70,8 +70,8 @@ def fd(L, eps=0.0001):
         H = maximum(np.random.randn(r, m), 0)
         theta = maximum(np.random.randn(r), 0)
 
-        Mu_W = maximum(np.random.randn(n, r), 0)
-        Mu_H = maximum(np.random.randn(r, m), 0)
+        Mu_W = maximum(np.random.randn(n, r), 0) 
+        Mu_H = maximum(np.random.randn(r, m), 0) 
         Mu_theta = maximum(np.random.randn(r), 0)
         
         assert np.allclose(fd_W(X, W, theta, H, L, Mu_W, Mu_theta, Mu_H, eps=eps), grad_W(X, W, theta, H, L, Mu_W, Mu_theta, Mu_H), atol=0.001)
@@ -82,24 +82,23 @@ def fd(L, eps=0.0001):
 def grad_W(X, W, theta, H, L, Mu_W,  Mu_theta, Mu_H):
     estim = W.dot(np.diag(theta)).dot(H)
     return -2*(X - estim).dot((np.diag(theta).dot(H)).T) \
-        + Mu_W * np.maximum(W, 0)
-
+        + np.maximum(W + Mu_W, 0)
 
 
 def grad_H(X, W, theta, H, L, Mu_W, Mu_theta, Mu_H):
     estim = W.dot(np.diag(theta)).dot(H)
     return ((W.dot(np.diag(theta))).T).dot(-2*(X - estim)) \
-        + Mu_H * np.maximum(H, 0)
+        + np.maximum(H + Mu_H, 0)
 
 
 def grad_theta(X, W, theta, H, L, Mu_W, Mu_theta, Mu_H):
     estim = W.dot(np.diag(theta)).dot(H)
     tmp = -2*(X - estim)
     return np.diag(np.dot(np.dot(W.T, tmp), H.T)) + L * np.sign(theta) \
-        + Mu_theta * np.maximum(theta, 0)
+        + np.maximum(theta + Mu_theta, 0)
 
     
-def opt(X, n, m, r, iters=200):
+def opt(X, n, m, r, iters=2000):
 
     X = maximum(np.random.randn(n, m), 0)
     W = maximum(np.random.randn(n, r), 0)
@@ -109,6 +108,8 @@ def opt(X, n, m, r, iters=200):
     Mu_W = ones_like(W)
     Mu_H = ones_like(H)
     Mu_theta = ones_like(theta)
+
+    rho = 0.1
     
     eta = 0.001
     
@@ -127,9 +128,17 @@ def opt(X, n, m, r, iters=200):
             # theta = np.maximum(theta, 0)
 
         print func(X, W, theta, H, L, Mu_W,  Mu_theta, Mu_H)
+
+        #Mu_W += rho * np.maximum(0, W)
+        #Mu_H += rho * np.maximum(0, H)
+        #Mu_theta += rho * np.maximum(0, theta)
+
         #dH = grad_H(X, W, theta, H)
         #    dtheta = grad_theta(X, W, theta, H)
 
+        print "W min", W.min()
+        #print "H min", Mu_H.min()
+        #print "theta min", Mu_theta.min()
 
 if __name__ == "__main__":
 
