99b596ec48184271a496f3148f48ce602c47e82b
Ryan Cotterell <ryan.cotterell@gmail.com>
2017-05-03 20:46:26 -0400

    minor

--- augmented-lagrangian/run.sh
+++ augmented-lagrangian/run.sh
@@ -5,6 +5,6 @@ do
     for eta in 0.01 0.1 0.5 1 10
     do
 	echo lambda=${lambda}, eta=${eta}
-	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log--L ${lambda} --eta ${eta} &
+	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log --L ${lambda} --eta ${eta} &
     done
 done
