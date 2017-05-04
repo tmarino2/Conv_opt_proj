e9ead704e73ae7efabbb3a03fb09bd02a124433c
Ryan Cotterell <ryan.cotterell@gmail.com>
2017-05-03 20:44:53 -0400

    update

---
+++ augmented-lagrangian/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for lambda in 0.01 0.1 0.5 10
+do
+    for eta in 0.01 0.1 0.5 1 10
+    do
+	echo lambda=${lambda}, eta=${eta}
+	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log--L ${lambda} --eta ${eta} &
+    done
+done
