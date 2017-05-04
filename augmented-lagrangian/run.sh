ea48ffa130f9fbb7663b066bf6df2e5a7ab93b26
Ryan Cotterell <rcotterell@a01.clsp.jhu.edu>
2017-05-04 10:47:31 -0400

    added some log files

--- augmented-lagrangian/run.sh
+++ augmented-lagrangian/run.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
 
-for lambda in 0.01 0.1 0.5 10
+	#!/bin/bash
+
+for lambda in 10.
 do
-    for eta in 0.01 0.1 0.5 1 10
+    for eta in 0.01 0.1 0.5 1
     do
 	echo lambda=${lambda}, eta=${eta}
-	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log --L ${lambda} --eta ${eta} &
+	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log2 --L ${lambda} --eta ${eta} &
+    done
+    for eta in 10
+    do
+	echo lambda=${lambda}, eta=${eta}
+	python opt.py --X ../data1.mat --log lambda=${lambda},eta=${eta}.log2 --L ${lambda} --eta ${eta} 
     done
 done
